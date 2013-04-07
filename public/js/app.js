36b6510e9c2c1064f30f806676991b84f2700c5d
Chris Schmitz <ccschmitz@gmail.com>
2013-04-07 18:46:18 -0500

    Add ability to select a transaction to view its details

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,49 @@
 jQuery(function($) {
 
-	var transactions = $.getJSON('js/fixtures/transactions.json', function(data) {
-		var items = [],
-		    $transactions = $('.transactions');
+  var $transactionsContainer = $('#transactions'),
+      $sidebar = $('#sidebar');
 
-		$.each(data, function(key, val) {
-			items.push('<li><a href="#" alt="-"><img src="'+ val.avatar +'"><date>'+ moment(val.date).format('MMMM').substring(0, 3) +'<br /><span>'+ moment(val.date).format('D').substring(0, 3) +'</span></date><detail><h6 class="title">'+ val.title +'</h6><i class="description">'+ val.description +'</i></detail><h5 class="amount '+ val.debit +'">'+ accounting.formatMoney(val.amount) +'</h5><ul class="tag"><li>'+ val.category +'</li></ul><div class="clear"></div><!-- .clear --></a></li>');
-		});
+  // setup the transactions when the page loads
+  $.getJSON('js/fixtures/transactions.json', function(data) {
+    var items = [];
 
-		$transactions.html(items);
-	});
+    $.each(data, function(key, val) {
+      var transactionDescription = val.description.substring(0, 26) + '...';
+          $transaction = $('<li class="transaction"><a href="#" alt="-"><img src="'+ val.avatar +'"><date>'+ moment(val.date).format('MMMM').substring(0, 3) +'<br /><span>'+ moment(val.date).format('D').substring(0, 3) +'</span></date><detail><h6 class="title">'+ val.title +'</h6><i class="description">'+ transactionDescription +'</i></detail><h5 class="amount '+ val.debit +'">'+ accounting.formatMoney(val.amount) +'</h5><ul class="tag"><li>'+ val.category +'</li></ul><div class="clear"></div><!-- .clear --></a></li>');
+
+      $transaction.find('a').data(val);
+
+      items.push($transaction);
+    });
+
+    $transactionsContainer.html(items);
+
+    // view individual transaction details
+    $transactionsContainer.find('li > a').on('click', function(e) {
+      inspectTransaction($(this));
+      return false;
+    });
+  });
+
+  function inspectTransaction(transaction) {
+    highlightTransactions(transaction);
+
+    // setup the transaction information in the sidebar
+    var $transactionHtml = '<h3>'+ transaction.data('title') +'</h3>';
+        $transactionHtml += '<date>'+ transaction.data('date') +'</date>';
+        $transactionHtml += '<p>'+ transaction.data('description') +'</p>';
+        $transactionHtml += '<ul><li>'+ transaction.data('category') +'</li></ul>';
+        $transactionHtml += '<location><img src="'+ transaction.data('location') +'"></location>';
+        $transactionHtml += '<img src="http://lorempixel.com/24/24" /> Transaction by ' + transaction.data('user');
+
+    $('#sidebar').fadeOut('fast', function() {
+      $(this).html($transactionHtml).fadeIn();
+    });
+  }
+
+  function highlightTransactions(transactions) {
+    $transactionsContainer.find('li > a').removeClass('highlight');
+    transactions.addClass('highlight');
+  }
 
 });
